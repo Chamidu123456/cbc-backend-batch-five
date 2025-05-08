e9ee6cd020095e33142792a9b180ae23c060d9dd
Chamidu <chamidupravinda456@gmail.com>
2025-05-08 12:00:33 +0530

    import cors module

--- index.js
+++ index.js
@@ -1,63 +1,54 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import productRouter from './routs/productRoute.js';
-import userRouter from './routs/userRoute.js';
-import orderRouter from './routs/orderRoute.js';
-import jwt from 'jsonwebtoken'
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import productRouter from "./routs/productRoute.js";
+import userRouter from "./routs/userRoute.js";
+import orderRouter from "./routs/orderRoute.js";
+import jwt from "jsonwebtoken";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use(
-    (req,res,next)=>{
-        const tokenString = req.header("Authorization")
-        if(tokenString != null){
-            const token = tokenString.replace("Bearer ", "")
-            
-            
-            jwt.verify(token, "cbc-batch-five#@2025" ,
-                (err,decoded)=>{
-                    if(decoded != null){
-                        console.log(decoded)
-                        req.user = decoded
-                        next()
-                    }else{
-                        console.log("invalid token")
-                        res.status(403).json({
-                            message: "Invalid token"
-                        })
-                    }
-                }
-            )
+app.use((req, res, next) => {
+  const tokenString = req.header("Authorization");
+  if (tokenString != null) {
+    const token = tokenString.replace("Bearer ", "");
 
-        }else{
-            next()
-        }
-        
-        
-    }
-)
-   
+    jwt.verify(token, "cbc-batch-five#@2025", (err, decoded) => {
+      if (decoded != null) {
+        console.log(decoded);
+        req.user = decoded;
+        next();
+      } else {
+        console.log("invalid token");
+        res.status(403).json({
+          message: "Invalid token",
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.rzdjnm7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://admin:123@cluster0.rzdjnm7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
     console.log("Connected to the database");
-}).catch(()=>{
+  })
+  .catch(() => {
     console.log("Database connection failed");
-})
+  });
 
+app.use("/products", productRouter);
+app.use("/users", userRouter);
+app.use("/orders", orderRouter);
 
-app.use("/products", productRouter)
-app.use("/users", userRouter)
-app.use("/orders", orderRouter)
-
-
-app.listen( 5000,
-    ()=>{
-        console.log("Server is running on port 5000");
-    }
-)
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
